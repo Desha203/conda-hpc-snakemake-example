43d47e153a8983d26c7b8a4ae4919b16f9d6b9ad
Desha Wallace <rb21991@bc4login1.bc4.acrc.priv>
2024-11-14 16:52:21 +0000

    fixed bug

--- code/0_get_conditions.R
+++ code/0_get_conditions.R
@@ -11,3 +11,8 @@ dta %>%
   head(4) %>%
   select(condition) %>%
   write_csv('../data/derived/top_conditions.csv')
+
+#create a csv which is a column of these conditions.
+#Snakemake needs this is a .yml format.
+#currently in .csv format
+#this is what the makecog.sh file is doing 
--- code/practical_2/make_config.sh
+++ code/practical_2/make_config.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 
+conditions_csv_f='../../data/derived/top_conditions.csv'
 
+only_conditions=$(sed '1d' $conditions_csv_f)
+conds=($only_conditions)
 
-echo "CONDITIONS:" > ../config.yml
-{       read
-        while IFS=, read -r line;
-        do
-                printf "  - ${line}\n">>../config.yml
-
-        done
-
-} < ../../data/derived/top_conditions.csv
+printf 'CONDITIONS:\n' >  ~/projects/conda-hpc-snakemake-example/code/config.yml
+for i in ${conds[@]}; do 
+  echo $i
+ printf "  - ${i}\n" >> ~/projects/conda-hpc-snakemake-example/code/config.yml
+done
 
